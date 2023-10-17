ac951750df825bdd8aad32b2f4066c8f343722a7
Jishnu <jishnu.kb17291@gmail.com>
2023-10-17 12:51:26 +0530

    initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ("username", "email", "age")  # edited
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "age")  # edited
